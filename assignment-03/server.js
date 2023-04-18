40073163233dd18a867f9f143697d1a80eb5b013
Festus Osayi <105089377+Festus-Osayi@users.noreply.github.com>
2023-04-18 04:02:54 -0400

    Update server.js

--- assignment-03/server.js
+++ assignment-03/server.js
@@ -1,4 +1,4 @@
-/**************** * WEB322 – Assignment 05 * 
+/**************** * WEB322 – Assignment 03 * 
 I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
 No part * of this assignment has been copied manually or electronically from any other source * 
 (including 3rd party web sites) or distributed to other students. 
