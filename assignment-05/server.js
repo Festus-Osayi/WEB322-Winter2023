625507bcaa5555799f6fce1d2f14749f0c70095f
Festus Osayi <105089377+Festus-Osayi@users.noreply.github.com>
2023-04-18 04:01:47 -0400

    Update server.js

--- assignment-05/server.js
+++ assignment-05/server.js
@@ -1,5 +1,12 @@
 'use strict'
-/********************************************************************************* * WEB322 – Assignment 05 * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part * of this assignment has been copied manually or electronically from any other source * (including 3rd party web sites) or distributed to other students. * * Name: __Festus Osayi____________________ Student ID: ___170276216___________ Date: ____2-17-2023____________ * * Cyclic Web App URL: _https://zany-gold-dove-tam.cyclic.app_____________________ ___________________________________________________ * * GitHub Repository URL: _____________________https://github.com/Festus-Osayi/web322-app_________________________________ * ********************************************************************************/
+/**************** * WEB322 – Assignment 05 * 
+I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
+No part * of this assignment has been copied manually or electronically from any other source * 
+(including 3rd party web sites) or distributed to other students. 
+* * Name: __Festus Osayi____________________ Student ID: ___170276216___________ Date: ____2-17-2023____________ 
+* * Cyclic Web App URL: _https://zany-gold-dove-tam.cyclic.app_____________________
+* * GitHub Repository URL: _____________________https://github.com/Festus-Osayi/web322-app
+* ********************************************************************************/
 const HTTP_PORT = process.env.PORT || 8080;
 const express = require("express");
 const multer = require('multer');
